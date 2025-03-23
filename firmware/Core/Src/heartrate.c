76642677736194ea9dfb0e78063d12c547a804bb
Sandro Petrovski <sandro@petrovski.cc>
2025-03-23 13:25:12 -0400

    add: begin sensor integration

--- firmware/Core/Src/heartrate.c
+++ firmware/Core/Src/heartrate.c
@@ -1,62 +1,50 @@
 #include "heartrate.h"
 
 extern I2C_HandleTypeDef hi2c1;
-extern UART_HandleTypeDef huart2;
+
+#define FIFO_CFG 0b10010000
+#define MODE_CFG 0b01000010
 
 #define MAX30102_ADDR  0xAE  // 7-bit address shifted left
 
-// Define global variables ONLY in `heartrate.c`
-uint32_t ir_buffer[50];
-uint8_t  buffer_index     = 0;
-uint32_t last_peak_time   = 0;
-uint8_t  bpm             = 0;
+#define FIFO_CFG_REG 0x08
+#define MODE_CFG_REG 0x09
+#define FIFO_WR_PTR_REG 0x04
+#define FIFO_RD_PTR_REG 0x06
 
-
-void MAX30102_Init() {
-    uint8_t mode_config[2] = {0x09, 0x03};  // SpO2 mode
-    uint8_t spo2_config[2] = {0x0A, 0x27};  // 100 samples/sec, 411us pulse width
-    uint8_t led_config[2] = {0x0C, 0x24};   // LED pulse amplitude
-
-    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR, mode_config, 2, 100);
-    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR, spo2_config, 2, 100);
-    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR, led_config, 2, 100);
+void hr_tx(uint8_t sub_addr, uint8_t *data, uint16_t size) {
+    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR | 0b0, &sub_addr, 1, HAL_MAX_DELAY);
+    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR | 0b0, data, size, HAL_MAX_DELAY);
 }
 
-uint32_t MAX30102_ReadFIFO() {
-    uint8_t reg = 0x07;
-    uint8_t data[6];
-
-    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR, &reg, 1, 100);
-    HAL_I2C_Master_Receive(&hi2c1, MAX30102_ADDR, data, 6, 100);
-
-    uint32_t ir_value = (data[0] << 16) | (data[1] << 8) | data[2];  // IR LED Data
-    return ir_value;
+void hr_rx(uint8_t sub_addr, uint8_t *data, uint16_t size) {
+    HAL_I2C_Master_Transmit(&hi2c1, MAX30102_ADDR | 0b0, &sub_addr, 1, HAL_MAX_DELAY);
+    HAL_I2C_Master_Receive(&hi2c1, MAX30102_ADDR | 0b1, data, size, HAL_MAX_DELAY);
 }
 
-void Process_HeartRate() {
-    static uint8_t beat_count = 0;
-    static uint32_t last_time = 0;
-    uint32_t current_time = HAL_GetTick();  // system time in ms
-
-    uint32_t ir_value = MAX30102_ReadFIFO();
-    ir_buffer[buffer_index++] = ir_value;
-
-    if (buffer_index >= 50) {
-        buffer_index = 0;
-    }
-
-    if (ir_value > 50000 && (current_time - last_time) > 300) {
-        beat_count++;
-        last_time = current_time;
-    }
-
-    if ((current_time - last_peak_time) > 10000) {
-        bpm = beat_count * 6;
-        beat_count = 0;
-        last_peak_time = current_time;
-    }
+void init_hr() {
+    uint8_t fifo_cfg = FIFO_CFG;
+    uint8_t mode_cfg = MODE_CFG;
+    hr_tx(MODE_CFG_REG, &mode_cfg, 1);
+    HAL_Delay(200);
+    hr_tx(FIFO_CFG_REG, &fifo_cfg, 1);
 }
 
-void Send_HeartRate_UART() {
-    printf("Heart Rate: %d BPM\r\n", bpm);
+uint8_t get_fifo_wr_ptr() {
+    uint8_t ptr;
+    hr_rx(FIFO_WR_PTR_REG, &ptr, 1);
+    return ptr;
+}
+
+uint8_t get_fifo_rd_ptr() {
+    uint8_t ptr;
+    hr_rx(FIFO_RD_PTR_REG, &ptr, 1);
+    return ptr;
+}
+
+void update_hr_data() {
+    const uint8_t num_samples = get_fifo_wr_ptr() - get_fifo_rd_ptr();
+    for (uint8_t i = 0; i < num_samples; i++) {
+        uint8_t hr_sample =
+    }
 }
