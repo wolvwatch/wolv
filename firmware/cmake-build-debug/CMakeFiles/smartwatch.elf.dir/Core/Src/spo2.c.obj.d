7634178e675480a0c8cd3e9a4e3d63e6afc422a5
Sandro Petrovski <sipetrov@umich.edu>
2025-04-07 16:36:13 -0400

    add: setpixels implementation

---
+++ firmware/cmake-build-debug/CMakeFiles/smartwatch.elf.dir/Core/Src/spo2.c.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/smartwatch.elf.dir/Core/Src/spo2.c.obj: \
+ C:\Users\Sandro\projects\smartwatch\firmware\Core\Src\spo2.c \
+ C:/Users/Sandro/projects/smartwatch/firmware/Core/Inc/spo2.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/arm-none-eabi/include/stdint.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/arm-none-eabi/include/machine/_default_types.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/arm-none-eabi/include/sys/features.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/arm-none-eabi/include/_newlib_version.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/arm-none-eabi/include/sys/_intsup.h \
+ C:/PROGRA~2/ARMGNU~1/14EFD8~1.2RE/arm-none-eabi/include/sys/_stdint.h
